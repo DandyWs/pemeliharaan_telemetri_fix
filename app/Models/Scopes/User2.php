ca87a4b6fbff418ff09697b503267275b24b4288
Aurasagita <aurasagita2122@gmail.com>
2024-11-18 14:15:38 +0700

    keri

---
+++ app/Models/Scopes/User2.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Laravel\Sanctum\HasApiTokens;
+use App\Models\Scopes\Searchable;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User2 extends Authenticatable
+{
+    use Notifiable;
+    use HasFactory;
+    use Searchable;
+    use HasApiTokens;
+    // use MustVerifyEmail;
+
+    protected $fillable = ['name', 'email', 'password', 'role', 'profile_photo_path'];
+
+    protected $searchableFields = ['*'];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+        'two_factor_secret',
+        'two_factor_recovery_codes',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'two_factor_confirmed_at' => 'datetime',
+    ];
+
+    public function pemeliharaans()
+    {
+        return $this->hasMany(Pemeliharaan2::class);
+    }
+
+    public function pemeriksaans()
+    {
+        return $this->hasMany(Pemeriksaan::class);
+    }
+
+    public function isSuperAdmin(): bool
+    {
+        return in_array($this->role, config('auth.super_admins'));
+    }
+}
