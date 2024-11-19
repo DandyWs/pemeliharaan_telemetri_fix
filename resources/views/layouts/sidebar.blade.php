88963ccec2fc55cc085b4d0785095a521e467fe2
serliputri <serliputrimaharani@gmail.com>
2024-11-19 13:26:05 +0700

    notifikasi

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -6,16 +6,14 @@
   <!-- Sidebar Menu -->
   <nav class="mt-2">
     <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-      <!-- Add icons to the links using the .nav-icon class
-           with font-awesome or any other icon font library -->
       <li class="nav-item ">
         <a href="{{url('/dashboard')}}" class="nav-link  {{request()->is('dashboard')?'active' : ''}}">
             <i class="nav-icon fas fa-tachometer-alt"></i>
             <p>
               Dashboard
             </p>
-          </a>
-        </li>
+        </a>
+      </li>
 
      <li class="nav-item ">
           <a href="#" class="nav-link">
