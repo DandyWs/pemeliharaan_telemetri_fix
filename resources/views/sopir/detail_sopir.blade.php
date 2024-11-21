41dcdbb13d65af142def44d8966a2b3e54f22ec3
DandyWs <Dandyws71@gmail.com>
2024-11-21 07:53:04 +0700

    update sopir

--- resources/views/sopir/detail_sopir.blade.php
+++ resources/views/sopir/detail_sopir.blade.php
@@ -11,7 +11,7 @@
         <div class="card-body">
             <ul class="list-group list-group-flush">
                 {{-- <li class="list-group-item"><b>Id Sopir : </b>{{$sopir->id}}</li> --}}
-                <li class="list-group-item"><b>Nama : </b>{{$sopir->nama}}</li>
+                <li class="list-group-item"><b>Nam : </b>{{$sopir->nama}}</li>
                 {{-- <li class="list-group-item"><b>Alamat : </b>{{$sopir->alamat}}</li>
                 <li class="list-group-item"><b>No. Telepon : </b>{{$sopir->phone}}</li> --}}
                 <a class="btn btn-md btn-primary" href="{{ url('/sopir') }}">Kembali</a>
